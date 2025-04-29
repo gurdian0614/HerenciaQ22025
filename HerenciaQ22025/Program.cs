74b8252220782756bea18b6e5f54202d949e1a28
gurdian0614 <ivan.gurdian@outlook.com>
2025-04-28 19:23:34 -0600

    Adding data

--- HerenciaQ22025/Program.cs
+++ HerenciaQ22025/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HerenciaQ22025;
+
+Vertebrado vertebrado = new Vertebrado();
+vertebrado.Nombre = "Tiburón Blanco";
+vertebrado.Edad = 15;
+vertebrado.Especie = "Carcharodon carcharias";
+vertebrado.Habitat = "Océano";
+vertebrado.TipoEsqueleto = "Cartilaginoso";
+vertebrado.NumeroExtremidades = 4;
+vertebrado.TipoSangre = "Sangre Fría";
+vertebrado.Dieta = "Carnívoro";
+vertebrado.ExpectativaVida = 70;
+vertebrado.Reproduccion = "Ovíparo";
+vertebrado.Imprimir();
+
+Console.WriteLine();
+
+Invertebrado invertebrado = new Invertebrado();
+invertebrado.Nombre = "Medusa";
+invertebrado.Edad = 3;
+invertebrado.Especie = "Aurelia aurita";
+invertebrado.Habitat = "Océano";
+invertebrado.TipoCuerpo = "Gelatinoso";
+invertebrado.MetodoReproduccion = "Asexual";
+invertebrado.TipoAlimentacion = "Filtrador";
+invertebrado.CicloVida = "Metamorfosis";
+invertebrado.Tamano = "Mediano";
+invertebrado.Imprimir();
