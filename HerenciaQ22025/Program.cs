adfee780505c21ab0c4b17c93e5faf1dd27c136b
gurdian0614 <ivan.gurdian@outlook.com>
2025-04-30 18:42:43 -0600

    Adding dynamic data

--- HerenciaQ22025/Program.cs
+++ HerenciaQ22025/Program.cs
@@ -1,28 +1,73 @@
 ﻿using HerenciaQ22025;
 
 Vertebrado vertebrado = new Vertebrado();
-vertebrado.Nombre = "Tiburón Blanco";
-vertebrado.Edad = 15;
-vertebrado.Especie = "Carcharodon carcharias";
-vertebrado.Habitat = "Océano";
-vertebrado.TipoEsqueleto = "Cartilaginoso";
-vertebrado.NumeroExtremidades = 4;
-vertebrado.TipoSangre = "Sangre Fría";
-vertebrado.Dieta = "Carnívoro";
-vertebrado.ExpectativaVida = 70;
-vertebrado.Reproduccion = "Ovíparo";
-vertebrado.Imprimir();
+Invertebrado invertebrado = new Invertebrado();
+int opcion;
 
+Console.WriteLine("********************************");
+Console.WriteLine("*             MENU             *");
+Console.WriteLine("* 1. Animal Vertebrado         *");
+Console.WriteLine("* 2. Animal Invertebrado       *");
+Console.WriteLine("********************************");
+Console.Write("Escoja la opción: ");
+opcion = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine();
 
-Invertebrado invertebrado = new Invertebrado();
-invertebrado.Nombre = "Medusa";
-invertebrado.Edad = 3;
-invertebrado.Especie = "Aurelia aurita";
-invertebrado.Habitat = "Océano";
-invertebrado.TipoCuerpo = "Gelatinoso";
-invertebrado.MetodoReproduccion = "Asexual";
-invertebrado.TipoAlimentacion = "Filtrador";
-invertebrado.CicloVida = "Metamorfosis";
-invertebrado.Tamano = "Mediano";
-invertebrado.Imprimir();
+switch(opcion)
+{
+    case 1:
+        Console.WriteLine("*********************************");
+        Console.WriteLine("*       ANIMAL VERTEBRADO       *");
+        Console.WriteLine("*********************************");
+        Console.Write("Nombre: ");
+        vertebrado.Nombre = Console.ReadLine();
+        Console.Write("Edad: ");
+        vertebrado.Edad = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Especie: ");
+        vertebrado.Especie = Console.ReadLine();
+        Console.Write("Hábitat: ");
+        vertebrado.Habitat = Console.ReadLine();
+        Console.Write("Tipo de Esqueleto: ");
+        vertebrado.TipoEsqueleto = Console.ReadLine();
+        Console.Write("Número de extremidades: ");
+        vertebrado.NumeroExtremidades = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Tipo de sangre: ");
+        vertebrado.TipoSangre = Console.ReadLine();
+        Console.Write("Dieta: ");
+        vertebrado.Dieta = Console.ReadLine();
+        Console.Write("Expectativa de vida: ");
+        vertebrado.ExpectativaVida = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Tipo de Reproducción: ");
+        vertebrado.Reproduccion = Console.ReadLine();
+        vertebrado.Imprimir();
+        break;
+
+    case 2:
+        Console.WriteLine("**********************************");
+        Console.WriteLine("*       ANIMAL INVERTEBRADO      *");
+        Console.WriteLine("**********************************");
+        Console.Write("Nombre: ");
+        invertebrado.Nombre = Console.ReadLine();
+        Console.Write("Edad: ");
+        invertebrado.Edad = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Especie: ");
+        invertebrado.Especie = Console.ReadLine();
+        Console.Write("Hábitat: ");
+        invertebrado.Habitat = Console.ReadLine();
+        Console.Write("Tipo de cuerpo: ");
+        invertebrado.TipoCuerpo = Console.ReadLine();
+        Console.Write("Método de reproducción: ");
+        invertebrado.MetodoReproduccion = Console.ReadLine();
+        Console.Write("Tipo de alimentación: ");
+        invertebrado.TipoAlimentacion = Console.ReadLine();
+        Console.Write("Ciclo de vida: ");
+        invertebrado.CicloVida = Console.ReadLine();
+        Console.Write("Tamaño: ");
+        invertebrado.Tamano = Console.ReadLine();
+        invertebrado.Imprimir();
+        break;
+
+    default:
+        Console.WriteLine("Opción no válida");
+        break;
+}
